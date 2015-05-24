eff486b046fedccb19adb6c86e1cd4f54a1a5e8b
Natallia Abramchuk <na2sia88@mail.ru>
2015-05-24 22:54:29 +0400

    1_2

---
+++ CheckPoint2_2/CheckPoint2_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CheckPoint2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separator = new char[10] { ';',' ','.',',',':','(',')','?','!','"'};
+
+            StreamReader inputFile = new StreamReader("test.txt", Encoding.Default);
+            string readFile;
+            int CountLine = 1;
+
+            Dictionary<string, Words> MyDictionary = new Dictionary<string, Words>();
+            
+            while ((readFile = inputFile.ReadLine()) != null)
+            {
+                var text = readFile.ToLower().Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string i in text)
+                { 
+                    Words word;
+                    if (!MyDictionary.TryGetValue(i, out word))
+                        word = MyDictionary[i] = new Words();
+                    word.Lines.Add(CountLine);
+                    word.CountWord++;
+                }
+                CountLine++;
+            }
+
+            using( var outputFile = new StreamWriter("dict.txt"))
+            foreach (var k in MyDictionary.OrderBy(x=>x.Key))
+            {
+                outputFile.WriteLine("{0}{1}: {2}", k.Key.PadRight(20, '.'), k.Value.CountWord, string.Join(" ", k.Value.Lines.OrderBy(l => l)));
+            }
+
+        }
+    }
+}
---
+++ CheckPoint2_2/CheckPoint2_2/Words.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckPoint2_2
+{
+    public class Words
+    {
+        public string Word{get;set;}
+        public int CountWord{get;set;}
+        public HashSet<int> Lines = new HashSet<int>();
+       // public string LineWord { get; set; }
+       // public Words (string word, int count, HashSet<int> lines)
+          // {   Word = word;
+            //  CountWord = count;
+              //LineWord.addline;            }
+        
+    }
+}
